96ab2fe542b1a448ae9cbe85a5677a9dfd054dea
Nils Fahle <nils.fahle@zinal-consulting.de>
2024-10-23 14:45:39 +0200

    vibrate

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
-import "virtual:fonts.css"
-import './style.css'
+import "virtual:fonts.css";
+import "./style.css";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("crack");
+  button?.addEventListener("click", () => {
+    setTimeout(() => {
+      button.click();
+    }, 100);
+    navigator.vibrate(100);
+  });
+});
